837f3df492ee04d6fba07ca1374c5ea55c129a9c
Kumar Raja <61628711+kumarraja2k05@users.noreply.github.com>
2022-08-24 15:56:14 +0530

    Update admin_choice.py

--- admin_choice.py
+++ admin_choice.py
@@ -1,6 +1,47 @@
+import admin
+
 class AdminOp:
     def list_of_op():
-        pass
+        print("Enter 1 to see if there is any ongoing elections: ")
+        print("If Not than Press 2 to start elections: ")
+        print("Press 3 to add party: ")
+        print("Press 4 to approve user: ")
+        print("Press 5 if user want to become admin: ")
+        print("Press 6 to close elections: ")
+        print("Press 7 to see Results: ")
+        try:
+            choice=int(input("Enter choice: "))
+        except:
+            print("Wrong Input!! Please enter again!")
+            list_of_op()
+        else:
+            match choice:
+                case 1: 
+                    checkOngoingElections()
+                
+                case 2:
+                    startElection()
+                    
+                case 3:
+                    addParty()
+                    
+                case 4:
+                    isApproved()
+                    
+                case 5:
+                    makeAdmin()
+                    
+                case 6:
+                    closeEections()
+                    
+                case 7:
+                    results()
+                case default:
+                    print("Invalid Input!!")
+                    print("Please try again!!")
+                    list_of_op()
+                
+                
 
     #print the list of all operations, get choice from admin.
     # validate the choice.

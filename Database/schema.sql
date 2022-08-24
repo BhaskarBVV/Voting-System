e279b9ad5acc6ebf7ce145a7ab98b6ddba46601c
Kumar Raja <61628711+kumarraja2k05@users.noreply.github.com>
2022-08-24 19:09:22 +0530

    Update schema.sql

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,4 +1,4 @@
- create table User (
+create table if not exists User (
  UserId int primary key,
  Name Varchar (50) not null,
  Father_Name Varchar (50) not null,
@@ -11,32 +11,31 @@
  gender varchar(7) not null
 );
 
-
-create table Role (
+create table if not exists Role (
  UserId int not null,
  RoleId int not null,
  foreign key (UserId) references User(UserId)
 );
 
- create table Approval (
+ create table if not exists Approval (
  UserId int not null,
  IsApproved bool not null,
  foreign key (UserId) references user(UserId)
 );
 
-create table VoteRecord (
+create table if not exists VoteRecord (
 UserId int not null,
 Vstatus bool not null,
 VoteId int not null,
 foreign key (UserId) references user(UserId)
 );
 
- create table Party (
+create table if not exists Party (
  PartyID int not null primary key,
  PartyName varchar(50) not null
 );
 
-create table Result(
+create table if not exists Result(
 PartyId int not null,
 ElectionYear int not null,
 votes int not null,

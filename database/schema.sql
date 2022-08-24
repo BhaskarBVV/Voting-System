8df47fa936fcad5637f99876be492f1ddd80dffb
Kumar Raja <61628711+kumarraja2k05@users.noreply.github.com>
2022-08-24 23:36:48 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,44 +1,44 @@
 create table if not exists User (
- UserId int primary key,
- Name Varchar (50) not null,
- Father_Name Varchar (50) not null,
- Aadhaar_number bigint not null unique,
+ user_id int primary key,
+ name Varchar (50) not null,
+ father_Name Varchar (50) not null,
+ aadhaar_number bigint not null unique,
  age int not null,
  contact bigint not null unique,
- Email varchar (50) not null unique,
+ email varchar (50) not null unique,
  city varchar (20) not null,
  passwd varchar (40) not null,
  gender varchar(7) not null
 );
 
 create table if not exists Role (
- UserId int not null,
- RoleId int not null,
- foreign key (UserId) references User(UserId)
+ user_id int not null,
+ role_id int not null,
+ foreign key (user_id) references User(user_id)
 );
 
- create table if not exists Approval (
- UserId int not null,
- IsApproved bool not null,
- foreign key (UserId) references user(UserId)
+create table if not exists Approval (
+ user_id int not null,
+ is_approved bool not null,
+ foreign key (user_id) references user(user_id)
 );
 
 create table if not exists VoteRecord (
-UserId int not null,
-Vstatus bool not null,
-VoteId int not null,
-foreign key (UserId) references user(UserId)
+user_id int not null,
+status bool not null,
+vote_id int not null,
+foreign key (user_id) references user(user_id)
 );
 
 create table if not exists Party (
- PartyID int not null primary key,
- PartyName varchar(50) not null
+ party_iD int not null primary key,
+ party_name varchar(50) not null
 );
 
 create table if not exists Result(
-PartyId int not null,
-ElectionYear int not null,
+party_id int not null,
+election_year int not null,
 votes int not null,
-primary key(PartyId,ElectionYear),
-foreign key (PartyId) references Party(PartyId)
+primary key(party_id,election_year),
+foreign key (party_id) references Party(party_id)
 );

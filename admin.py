ed8058e2eb97777221f993dc18bcaef7ffd75c2d
Kumar Raja <61628711+kumarraja2k05@users.noreply.github.com>
2022-08-24 15:41:11 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 23 23:30:27 2022
-
-@author: kumar
-"""
 
 class Admin:
     def __init__():
@@ -14,6 +9,14 @@ class Admin:
         #if yes than only add user or allow user to login or register
         pass
     
+    def startElection():
+        # start elections
+        pass
+    
+    def addParty():
+        #add party for ongoing elections basically update party table
+        pass
+    
     def isApproved():
         #check if elections are ongoing tha approve 
         # else not approve and update approval table
@@ -21,14 +24,6 @@ class Admin:
     
     def makeAdmin():
         #ask to become admin
-        pass
-    
-    def addParty():
-        #add party for ongoing elections basically update party table
-        pass
-    
-    def startElection():
-        # start elections
         pass
     
     def closeEections():

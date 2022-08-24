cb2caf0bb6740812c60bcf338851c63a407ac2ba
Kumar Raja <61628711+kumarraja2k05@users.noreply.github.com>
2022-08-24 14:04:21 +0530

    Update Schema.sql

--- Database/Schema.sql
+++ Database/Schema.sql
@@ -1,24 +1,26 @@
-create table User (
+ create table User (
  UserId int primary key,
- Name Varchar (25) not null,
- Father_Name Varchar (25) not null,
- Aadhaar_number int not null unique,
+ Name Varchar (50) not null,
+ Father_Name Varchar (50) not null,
+ Aadhaar_number bigint not null unique,
  age int not null,
- contact int not null unique,
- Email varchar (20) not null unique,
+ contact bigint not null unique,
+ Email varchar (50) not null unique,
  city varchar (20) not null,
  passwd varchar (40) not null,
  gender varchar(7) not null
 );
 
+desc user;
 insert into user values(1,"Bhaskar","Kanhiya Lal",432893,22,1234567890,"abc@gmail.com","Hathras", "1234","Male");
-
+select * from User;
 
 create table Role (
  UserId int not null,
  RoleId int not null,
  foreign key (UserId) references User(UserId)
 );
+desc Role;
 
  create table Approval (
  UserId int not null,
@@ -35,5 +37,13 @@ foreign key (UserId) references user(UserId)
 
  create table Party (
  PartyID int not null primary key,
- PartyName varchar(20) not null
+ PartyName varchar(50) not null
+);
+
+create table Result(
+PartyId int not null,
+ElectionYear int not null,
+votes int not null,
+primary key(PartyId,ElectionYear),
+foreign key (PartyId) references Party(PartyId)
 );

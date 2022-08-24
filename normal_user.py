70710819258f4f73d367204d37b42e9441483656
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2022-08-24 16:26:11 +0530

    Add files via upload

---
+++ normal_user.py
@@ -0,0 +1,24 @@
+class User:
+    def __init__():
+        # User table initiallize
+        pass
+    
+    def editDetails():
+        #If user want to edit details
+        pass
+    
+    def giveVote():
+        # Give vote to the party
+        pass
+    
+    def isApprovedByAdmin():
+        # is apporved by admin to login or register
+        pass
+    
+    def becomeAdmin():
+        #Request to become admin
+        pass
+    
+    
+    
+    

dba1588aa9eeaeab05249966e0fbc8889436b2f8
BhaskarBVV <bhaskar.nv3@gmail.com>
2022-08-25 16:43:03 +0530

    little changes

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,5 @@
 insert into User values(1,"Admin","father",000000000000,22,1234567890,"abc@gmail.com","XYZ", "$2b$12$n4itbxM29qrVyZ9lkDSaeO77agVfXcsj/wOX7Oe64Wkg6fk3hJ5Ca","M");
 insert into Role values(1,1);
 insert into Approval values(1,1);
+
+-- insertion commands

c7f5fa4f5a3e6adcd3249b4dfbe3fa0fa50c7311
BhaskarBVV <bhaskar.nv3@gmail.com>
2022-08-27 18:02:08 +0530

    Table format

--- user/user_choice.py
+++ user/user_choice.py
@@ -1,8 +1,11 @@
+from tabulate import tabulate
 class options:
 
     def get_choice(available_op):
-        for idx,i in enumerate(available_op,0):
-            print(f"Enter {idx} to {i}")
+        # for idx,i in enumerate(available_op,0):
+        #     print(f"Enter {idx} to {i}")
+        print("Select from Available options : ")
+        print(tabulate([[idx,x] for idx,x in enumerate(available_op,0)], headers=["Enter","Operation"],tablefmt='fancy_grid'))
         choice=input("Enter your choice : ")
         try:
             choice=int(choice)
